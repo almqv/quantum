dc8a2f1ae06def02121102ed5f2d92e3e159e8de
AlmTech Software <elias@almtech.se>
2019-11-16 03:34:45 +0100

    removed unneeded comments in code

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -15,7 +15,7 @@ if SERVER then
     local path = "gamemodes/" .. GM.FolderName .. "/gamemode/content/materials/quantum/server_banner.png"
     resource.AddSingleFile( path )
 
-    include( "shared.lua" ) -- git push test (REMOVE THIS LATER)
+    include( "shared.lua" ) 
 
     Quantum.Server = {}
     include( "settings/sv_settings.lua" ) -- include the settings
