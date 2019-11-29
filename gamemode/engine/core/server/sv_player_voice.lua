83e95f2d3b48033e8d11264ecf7709248e2ac16d
AlmTech Software <h1g.server@gmail.com>
2019-11-29 17:57:28 +0100

    Fixed voice chat bug

--- gamemode/engine/core/server/sv_player_voice.lua
+++ gamemode/engine/core/server/sv_player_voice.lua
@@ -6,5 +6,5 @@
 --   \_\ /_/    \_\_|_| |_| |_|_|\___|\___|_| |_| /_/  
 
 function GM:PlayerCanHearPlayersVoice( lis, talk )
-	if( lis:GetPos():Distance( talk:GetPos() ) > Quantum.Server.Settings.VoiceChatRange ) then return false, true end
+	return lis:GetPos():Distance( talk:GetPos() ) <= Quantum.Server.Settings.VoiceChatRange, true
 end
